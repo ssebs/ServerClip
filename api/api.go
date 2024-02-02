1938d9b77ff2c085c1f6c3c4a5bc2352d5776e6b
Sebastian Safari <sebsafari@meta.com>
2024-02-02 09:30:27 -0800

    web server start

--- api/api.go
+++ api/api.go
@@ -1,1 +1,30 @@
 package api
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type API struct {
+	router *gin.Engine
+	port   int
+}
+
+func NewAPI(port int) *API {
+	r := gin.Default()
+
+	r.GET("/", rootHandler)
+
+	return &API{router: r, port: port}
+}
+
+func (a *API) Run() error {
+	return a.router.Run(fmt.Sprintf("0.0.0.0:%d", a.port))
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"status": "OK",
+	})
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/ssebs/ServerClip/api"
+)
 
 func main() {
 	fmt.Println("ServerClip")
 
+	a := api.NewAPI(5000)
+	log.Fatal(a.Run())
 }
